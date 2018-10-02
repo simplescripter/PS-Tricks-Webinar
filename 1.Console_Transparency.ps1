e09fc3bb2eed055f7e7581a1e521f85e763555e0
simplescripter <simplescripter@gmail.com>
2018-10-02 09:57:21 -0600

    Added 1.Console_Transparency.ps1

---
+++ 1.Console_Transparency.ps1
@@ -0,0 +1,4 @@
+# Use the PowerShell console menu --> Properties --> Colors tab to set console Opacity in Windows 10 and Server 2016
+
+# No code in this demo, but for a great function that can set console transparency programmatically, 
+# see Mathias R. Jessen's post here: https://stackoverflow.com/questions/39960519/is-it-possible-to-set-powershell-opacity-through-commandline
--- 4.SearchingAD.ps1
+++ 4.SearchingAD.ps1
@@ -1,3 +1,3 @@
 # Return a list of machines grouped by operating system
 
-Get-ADComputer -Filter * -Properties * | Sort OperatingSystem, LastLogonDate | Format-Table Name, IPv4Address, Enabled, LastLogonDate -GroupBy OperatingSystem
+Get-ADComputer -Filter * -Properties * | Sort-Object OperatingSystem, LastLogonDate | Format-Table Name, IPv4Address, Enabled, LastLogonDate -GroupBy OperatingSystem
