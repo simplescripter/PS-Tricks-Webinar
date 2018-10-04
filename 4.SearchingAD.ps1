030ba4a8e190371db5cda4db085922c142631e6b
simplescripter <simplescripter@gmail.com>
2018-10-04 10:18:53 -0600

    Updated SearchingAD.ps1

--- 4.SearchingAD.ps1
+++ 4.SearchingAD.ps1
@@ -1,11 +1,17 @@
-# Find stale user accounts
+# Find stale user accounts:
 
+Get-ADUser -Filter * -Properties LastLogonDate | Sort-Object LastLogonDate -Descending | Select-Object Name,LastLogonDate
 
+# Find Locked-Out Sales User Accounts and Unlock Them:
 
-# Return a list of machines
+Search-ADAccount -LockedOut -SearchBase 'OU=Sales,DC=Adatum,DC=Com' | Select-Object DistinguishedName,LockedOut,LastLogonDate
 
-Get-ADComputer -Filter * | Select -ExpandProperty Name
+Search-ADAccount -LockedOut -SearchBase 'OU=Sales,DC=Adatum,DC=Com' | ForEach-Object {Unlock-ADAccount -Identity $_.DistinguishedName}
 
-# Return a list of machines grouped by operating system
+# Return a list of machines:
+
+Get-ADComputer -Filter * | Select-Object -ExpandProperty Name
+
+# Return a list of machines grouped by operating system:
 
 Get-ADComputer -Filter * -Properties * | Sort-Object OperatingSystem, LastLogonDate | Format-Table Name, IPv4Address, Enabled, LastLogonDate -GroupBy OperatingSystem
