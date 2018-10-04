801af5b2ab582575eaf6c198348e44df9d0e45f2
simplescripter <simplescripter@gmail.com>
2018-10-04 15:06:59 -0600

    Added WorkingWithCollectionsOfSystems.ps1

---
+++ 5.WorkingWithCollectionsOfSystems.ps1
@@ -0,0 +1,19 @@
+# Query AD for a list of computer names:
+
+Get-ADComputer -Filter * | Select-Object -ExpandProperty Name
+
+# Store the list in a variable:
+
+$systems = Get-ADComputer -Filter * | Select-Object -ExpandProperty Name
+
+# Save the list to a file:
+
+Get-ADComputer -Filter * | Select-Object -ExpandProperty Name | Out-File E:\systems.txt
+
+# Create the list from a network query and store it in a variable:
+
+$systems = (net view | Select-String '\\\\') -replace '\\',''
+
+# Import the computer names from a .CSV file and store the names in a variable:
+
+$systems = Import-Csv E:\systems.CSV
